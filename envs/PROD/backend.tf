0a097156b0427e9489a842fc1fe36602223b79cb
Ramandeep Singh <reyman.raman@gmail.com>
2025-12-23 02:28:45 +0530

    adding prod environment

---
+++ envs/PROD/backend.tf
@@ -0,0 +1,29 @@
+/*resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "Terraform_VPC"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "Dev"
+  }
+}*/
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform.tfstate2025"
+    key            = "production"
+    region         = "us-east-1"
+    dynamodb_table = "Terraform_VPC"
+    encrypt        = true
+  }
+}
---
+++ envs/PROD/terraform.tfvars
@@ -0,0 +1,20 @@
+region   = "us-east-1"
+VPC_NAME = "vprofile_production"
+PROJECT  = "vprofile_production"
+
+Zone1 = "us-east-1a"
+Zone2 = "us-east-1b"
+Zone3 = "us-east-1c"
+
+VpcCIDR = "10.0.0.0/16"
+
+PubSub1CIDR = "10.0.10.0/24"
+PubSub2CIDR = "10.0.20.0/24"
+PubSub3CIDR = "10.0.30.0/24"
+
+PrivSub1CIDR = "10.0.40.0/24"
+PrivSub2CIDR = "10.0.50.0/24"
+PrivSub3CIDR = "10.0.60.0/24"
+
+trusted_ip    = "49.207.50.91/32"
+instance_type = "t2.micro"
