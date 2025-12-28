452ba7e68132b528cd13431732250d6df771d7a9
Ramandeep Singh <reyman.raman@gmail.com>
2025-12-29 01:55:14 +0530

    Ansible Installation using PIP

--- modules/ec2/installation_scripts/ansible.sh
+++ modules/ec2/installation_scripts/ansible.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
+set -euxo pipefail
 exec > /var/log/user-data.log 2>&1
-set -x
 
-# Run as "root" user
-sudo apt-get update -y
-sudo apt install -y software-properties-common git openssh-client
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
-ansible --version
-
-# Configure Ansible
-cd /etc/ansible
-sudo mv ansible.cfg ansible.cfg_bkp
-sudo -i -u root bash -c 'ansible-config init --disabled -t all > /etc/ansible/ansible.cfg'
-sudo sed -i 's/^;host_key_checking=True/host_key_checking=False/' ansible.cfg
-
-# Set hostname
-hostnamectl set-hostname ansible-controller
-
-# Enable password authentication for SSH (required for Jenkins Slave connection)
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config.d/*.conf
-systemctl restart ssh
-
-# --- Customize root prompt ---
+# --- Customize prompt ---
 echo "PS1='\[\e[0;32m\]\u\[\e[0m\]@\[\e[0;35m\]\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '" >> /root/.bashrc
-
-# --- Customize ubuntu prompt ---
 sudo -u ubuntu bash -c 'echo "PS1=\"\[\e[0;32m\]\u\[\e[0m\]@\[\e[0;35m\]\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ \"" >> ~/.bashrc'
 
-# Clone Vprofile repository
-#sudo -i -u ubuntu bash -c '
-#  mkdir -p ~/vprofile &&
-#  cd ~/vprofile &&
-#  mkdir -p ~/.ssh &&
-#  ssh-keyscan github.com >> ~/.ssh/known_hosts &&
-#  git clone https://github.com/reyman84/ansible.git
-#'
+# Hostname
+hostnamectl set-hostname ansible-controller
+
+######################################
+#  Ansible installation using "pip"  #
+######################################
+
+# Update system
+apt-get update -y
+
+# Install dependencies
+apt-get install -y python3 python3-pip python3-venv build-essential software-properties-common
+
+# Create working dir
+mkdir -p /root/ansible
+cd /root/ansible
+
+# Create venv (NO virtualenv command)
+python3 -m venv DEV
+
+# Install ansible INSIDE venv (NO source)
+./DEV/bin/pip install --upgrade pip
+./DEV/bin/pip install ansible
+
+# Verify explicitly
+./DEV/bin/ansible --version
