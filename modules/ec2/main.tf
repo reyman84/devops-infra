8a7f0eb3fe971e3d2a396deedea60d90524f7c73
Ramandeep Singh <reyman.raman@gmail.com>
2025-12-28 11:00:44 +0530

    updating subnet

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "this" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
-  subnet_id              = var.subnet_ids[count.index % length(var.subnet_ids)]
+  subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_group_ids
   associate_public_ip_address = true
 
