a572e01a78183e7a10bf605cd6c1e0b878d88fa4
Ramandeep Singh <reyman.raman@gmail.com>
2025-12-28 18:12:59 +0530

    Commenting

--- modules/security_groups/outputs.tf
+++ modules/security_groups/outputs.tf
@@ -2,17 +2,17 @@
 #           Security Group Outputs
 ##########################################
 
-output "allow_all_sg_id" {
+/*output "allow_all_sg_id" {
   description = "Allow all traffic security group ID"
   value       = aws_security_group.allow_all.id
-}
+}*/
 
 output "ssh_sg_id" {
   description = "SSH security group ID"
   value       = aws_security_group.ssh.id
 }
 
-output "jenkins_master_sg_id" {
+/*output "jenkins_master_sg_id" {
   description = "Jenkins master security group ID"
   value       = aws_security_group.jenkins_master.id
 }
@@ -25,4 +25,4 @@ output "nexus_sg_id" {
 output "sonarqube_sg_id" {
   description = "SonarQube security group ID"
   value       = aws_security_group.sonarqube.id
-}
+}*/
