a47777c836229f96891df0ec3fba23f0608ac5e9
Ramandeep Singh <reyman.raman@gmail.com>
2025-12-28 10:59:37 +0530

    Adding ansible module

--- envs/DEV/main.tf
+++ envs/DEV/main.tf
@@ -26,16 +26,97 @@ module "security_groups" {
   trusted_ip = var.trusted_ip
 }
 
-module "docker" {
+/*module "docker" {
   source = "../../modules/ec2"
 
-  name               = "${var.PROJECT}-ec2"
+  for_each = {
+    #docker1 = module.vpc.public_subnets[0]
+    #docker2 = module.vpc.public_subnets[1]
+    #docker3 = module.vpc.public_subnets[2]
+  }
+
+  name               = "${var.PROJECT}-${each.key}"
   ami_id             = data.aws_ami.ubuntu_24.id
   instance_type      = var.instance_type
   key_name           = aws_key_pair.dev.key_name
-  subnet_ids         = module.vpc.public_subnets
-  security_group_ids = [module.security_groups.ssh_sg_id, module.security_groups.allow_all_sg_id]
+  subnet_id          = each.value
+  security_group_ids = [ module.security_groups.ssh_sg_id, module.security_groups.allow_all_sg_id ]
 
   user_data = file("../../modules/ec2/installation_scripts/docker_install.sh")
+}*/
+
+/*module "jenkins_master" {
+  source = "../../modules/ec2"
+
+  name               = "jenkins-master"
+  ami_id             = data.aws_ami.ubuntu_24.id
+  instance_type      = "t3.large"
+  subnet_id          = module.vpc.public_subnets[0]
+  security_group_ids = [module.security_groups.jenkins_sg_id]
+  key_name           = aws_key_pair.dev.key_name
+
+  instance_count = 1
+
+  root_block_device = {
+    volume_size = 50
+    volume_type = "gp3"
+  }
+
+  ebs_block_devices = [
+    {
+      device_name = "/dev/sdf"
+      volume_size = 150
+      volume_type = "gp3"
+    }
+  ]
+
+  user_data = file("jenkins_master.sh")
 }
 
+
+module "jenkins_agent" {
+  source = "../../modules/ec2"
+
+  name               = "jenkins-agent"
+  ami_id             = data.aws_ami.ubuntu_24.id
+  instance_type      = "t3.medium"
+  subnet_id          = module.vpc.public_subnets[1]
+  security_group_ids = [module.security_groups.jenkins_agent_sg_id]
+  key_name           = aws_key_pair.dev.key_name
+
+  instance_count = 2
+
+  root_block_device = {
+    volume_size = 40
+    volume_type = "gp3"
+  }
+
+  ebs_block_devices = [
+    {
+      device_name = "/dev/sdf"
+      volume_size = 80
+      volume_type = "gp3"
+    }
+  ]
+
+  user_data = file("jenkins_agent.sh")
+}*/
+
+module "ansible" {
+  source = "../../modules/ec2"
+
+  for_each = {
+    ansible1 = module.vpc.public_subnets[0]
+    #ansible2 = module.vpc.public_subnets[1]
+    #ansible3 = module.vpc.public_subnets[2]
+  }
+
+  name               = "${var.PROJECT}-${each.key}"
+  ami_id             = data.aws_ami.ubuntu_24.id
+  instance_type      = var.instance_type
+  key_name           = aws_key_pair.dev.key_name
+  subnet_id          = each.value
+  security_group_ids = [ module.security_groups.ssh_sg_id ]
+
+  #user_data = file("../../modules/ec2/installation_scripts/docker_install.sh")
+}
