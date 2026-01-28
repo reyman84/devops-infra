3a2db2bfb10a8c7d2268e4311a0c2752170aeefe
Ramandeep Singh <58800265+reyman84@users.noreply.github.com>
2026-01-28 11:54:52 +0530

    Update in AZs

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,7 +4,7 @@ module "this" {
   name = var.VPC_NAME
   cidr = var.VpcCIDR
 
-  azs             = var.azs
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
   public_subnets  = var.public_subnets
   #private_subnets = var.private_subnets
 
