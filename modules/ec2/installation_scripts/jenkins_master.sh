1c4fe77cb87bb7c4b59390bc6a929eb5fce5252d
Ramandeep Singh <reyman.raman@gmail.com>
2026-06-09 15:58:46 +0530

    Update in Jenkins master installation

--- modules/ec2/installation_scripts/jenkins_master.sh
+++ modules/ec2/installation_scripts/jenkins_master.sh
@@ -1,48 +1,63 @@
 #!/bin/bash
 
-# Update system
-apt update -y
-apt install -y fontconfig ca-certificates apt-transport-https curl unzip
+# --- Logging --- #
+exec > >(tee /var/log/user-data.log)
+exec 2>&1
+set -euxo pipefail
 
-# Install AWS CLI v2
+# --- Update OS --- #
+apt update -y
+apt install -y curl unzip gnupg fontconfig ca-certificates apt-transport-https
+
+# --- Install AWS CLI v2 --- #
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+aws --version
 
-# Install Java 21
+# --- Install Java 21 --- #
 apt install -y openjdk-21-jdk
 java -version
 
-# Set JAVA_HOME globally
+# Set JAVA_HOME globally --- # 
 echo 'export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64' | sudo tee -a /etc/profile
 echo 'export PATH=$PATH:$JAVA_HOME/bin' | sudo tee -a /etc/profile
-
-# Apply JAVA_HOME for current session
+   
+# Apply JAVA_HOME for current session --- #
 export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 export PATH=$PATH:$JAVA_HOME/bin
 
-# Install Jenkins repo
+# --- Install Jenkins Repository --- #
+# Jenkins Key
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
-  -o /etc/apt/keyrings/jenkins-keyring.asc
 
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian-stable binary/" | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+gpg --keyserver keyserver.ubuntu.com \
+    --recv-keys 7198F4B714ABFC68
 
-apt update -y
-apt install -y jenkins
+gpg --export 7198F4B714ABFC68 \
+| gpg --dearmor \
+> /etc/apt/keyrings/jenkins.gpg
 
-# Download backup (IAM role must be attached!)
-aws s3 cp s3://jenkins-config-terraform/jenkins_backup_v1.tar.gz /root/jenkins_backup.tar.gz --region us-east-1
+# Jenkins Repository
+echo "deb [signed-by=/etc/apt/keyrings/jenkins.gpg] https://pkg.jenkins.io/debian-stable binary/" \
+> /etc/apt/sources.list.d/jenkins.list
 
-# Restore Jenkins config
-systemctl stop jenkins
-tar -xzvf /root/jenkins_backup.tar.gz -C / --overwrite
-chown -R jenkins:jenkins /var/lib/jenkins
-rm /root/jenkins_backup.tar.gz
+# Verify
+cat /etc/apt/sources.list.d/jenkins.list
+gpg --show-keys /etc/apt/keyrings/jenkins.gpg
+
+
+# --- Install Jenkins --- #
+apt-get update -y
+apt-get install -y jenkins
+
+# --- Start Jenkins --- #
+systemctl daemon-reload
+systemctl enable jenkins
 systemctl start jenkins
+systemctl status jenkins --no-pager
 
-# Set hostname
+# --- Set Hostname --- #
 hostnamectl set-hostname jenkins-master
 
 # Enable password authentication for SSH (required for Jenkins Slave connection)
@@ -50,8 +65,12 @@ sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/ssh
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config.d/*.conf
 systemctl restart ssh
 
-# --- Customize root prompt ---
+# --- Customize root prompt --- #
 echo "PS1='\[\e[0;32m\]\u\[\e[0m\]@\[\e[0;35m\]\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '" >> /root/.bashrc
 
-# --- Customize ubuntu prompt ---
+# --- Customize ubuntu prompt --- #
 sudo -u ubuntu bash -c 'echo "PS1=\"\[\e[0;32m\]\u\[\e[0m\]@\[\e[0;35m\]\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ \"" >> ~/.bashrc'
+
+# --- Jenkins Initial Password --- #
+echo "Jenkins initial password:"
+cat /var/lib/jenkins/secrets/initialAdminPassword
